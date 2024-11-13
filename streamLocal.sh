c47b27dc9e844204bc0d7c507007c2a93194459e
rvm <vrepiev@gmail.com>
2024-11-13 04:39:52 +0000

    fix stream local sh

--- streamLocal.sh
+++ streamLocal.sh
@@ -4,10 +4,10 @@ while true ; do
 
 /usr/bin/ffmpeg -f video4linux2 -i /dev/video0 \
 \
--filter_complex "amovie='/home/pi/keygen.mp3':loop=999,asetpts=N/SR/TB,\
+-filter_complex "amovie='./sound.mp3':loop=999,asetpts=N/SR/TB,\
 aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,volume=0.8" \
 \
--vf drawtext="fontfile=OpenSans.ttf:textfile='/home/pi/sensors.txt':reload=1:fontsize=28:\
+-vf drawtext="fontfile=OpenSans.ttf:textfile='./sensors.txt':reload=1:fontsize=18:\
 fontcolor=white:borderw=3:bordercolor=black:x=10:y=10" \
 \
 -vcodec h264_omx -preset ultrafast \
@@ -15,7 +15,7 @@ fontcolor=white:borderw=3:bordercolor=black:x=10:y=10" \
 -bf 1 -b_strategy 0 -sc_threshold 0 -pix_fmt yuv420p \
 -c:a aac -b:a 128k -b:v 16000k \
 \
--f flv rtmp://192.168.10.200/streamer/moe_okno
+-f flv rtmp://localhost:80/streamer/moe_okno
 
 done
 }
