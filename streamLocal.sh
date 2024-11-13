3ff8a0e413dbdf4cbe367f3aa0a96338158ba282
rvm <vrepiev@gmail.com>
2024-11-13 07:14:46 +0000

    fix for Pi OS Bullseye

--- streamLocal.sh
+++ streamLocal.sh
@@ -10,12 +10,12 @@ aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo,volume=0.8" \
 -vf drawtext="fontfile=OpenSans.ttf:textfile='./sensors.txt':reload=1:fontsize=18:\
 fontcolor=white:borderw=3:bordercolor=black:x=10:y=10" \
 \
--vcodec h264_omx -preset ultrafast \
--c:v h264_omx -x264opts "keyint=24:min-keyint=24:no-scenecut" -r 24 \
+-vcodec h264_v4l2m2m -profile:v 66 -preset ultrafast \
+-c:v h264_v4l2m2m -x264opts "keyint=24:min-keyint=24:no-scenecut" -r 24 \
 -bf 1 -b_strategy 0 -sc_threshold 0 -pix_fmt yuv420p \
 -c:a aac -b:a 128k -b:v 16000k \
 \
--f flv rtmp://localhost:80/streamer/moe_okno
+-f flv rtmp://192.168.1.52/streamer/moe_okno
 
 done
 }
