9264243f892159d25ab11aaa64bb0033db47f711
vrepiev@gmail.com <vreoiev@gmail.com>
2024-12-08 09:44:37 +0000

    add python config

--- readTemperature.sh
+++ readTemperature.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/bash
-source ~/./venv/bin/activate
+#!/usr/bin/env bash
+set -e
+if [ -z "$BASEDIR" ]; then
+  BASEDIR="/home/pi/raspberry-pi-secure-cam"
+fi
+
+source "$BASEDIR/launch_env.sh"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+source ~/.venv/bin/activate
+
 python3 temperText.py
 
