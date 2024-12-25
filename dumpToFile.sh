8088229422be927f6554a93654817ab288152bb1
rvm <vrepiev@gmail.com>
2024-12-25 05:04:26 +0000

    Add link to saved videos

--- dumpToFile.sh
+++ dumpToFile.sh
@@ -2,5 +2,5 @@
 
 /usr/bin/ffmpeg -hide_banner -i rtmp://localhost/streamer/live -c copy -flags +global_header \
 \
--f segment -strftime 1 -segment_time 60 -segment_format_options movflags=+faststart -reset_timestamps 1 CAM1_%Y-%m-%d_%H-%M-%S.mp4 
+-f segment -strftime 1 -segment_time 60 -segment_format_options movflags=+faststart -reset_timestamps 1 /data/video/CAM1_%Y-%m-%d_%H-%M-%S.mp4 
 
